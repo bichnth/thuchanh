4ce1b825a47a7503ee5ac375d85db9625d93f745
bichnth <132417455+bichnth@users.noreply.github.com>
2025-02-25 07:27:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function login()
 {
-  document.getElementById(compiler).style.display="block"
+  document.getElementById(compiler).style.display="block";
   let u = username.value
   let p = pw.value
   if (u=="thanhphu" && p="123")
