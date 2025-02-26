39defc604505f1d3d41e08999b596f70b8bffbf1
bichnth <132417455+bichnth@users.noreply.github.com>
2025-02-26 14:19:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = pw.value
-  if (u=="abc" && p=="123")
+  if (u == "abc" && p == "123")
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block";
